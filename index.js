f654673ee2328441877ad7280acc858cc933773f
DIBAKAR DAS <careofdibakar@gmail.com>
2024-04-04 23:40:51 +0530

    fs module exported and used in index file

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-const { log } = require('console');
-const http = require('http');
-const url = require('url');
+const { log } = require('console')
+const http = require('http')
+const url = require('url')
+const file = require('./fsModule')
 
 const server = http.createServer((request, response) => {
-    let requestURL = url.parse(request.url, true);
-    // response.write(requestURL.path); 
-    // response.write(requestURL.host); 
-    switch (requestURL.path) {
-        case '/home':
-            response.write('This is home page');
-            break;
-        case '/contact_us':
-            response.write('This is contact page');
-            break;
+    let requestURL = url.parse(request.url, true)
+    // log(requestURL) 
+    // response.write(request.method) 
+    // response.write(request.url) 
+    switch (requestURL.pathname) {
+        case '/create_file':
+            let body = ''
+
+            request.on('data', (chunk) => {
+                body += chunk
+            })
+            request.on('end', () => {
+                jsonData = JSON.parse(body)
+                // console.log('Received data:', jsonData)
+                file.createFile(jsonData.file_name, jsonData.file_content)
+            })
+
+            break
+        case '/read_file':
+            const fileContent = file.readFile(requestURL.query.file_name)
+            log(fileContent)
+            break
         case '/user':
-            response.write('This is user page');
-            break;
-    };
-    response.end();
-}).listen(8000, ()=>{
-    log('Server listening at 8000');
+            response.write('This is user page')
+            break
+    }
+    response.end()
+}).listen(8000, () => {
+    log('Server listening at 8000')
 })
 
-// server.listen(port, hostname, backlog, callback);
+// server.listen(port, hostname, backlog, callback)
