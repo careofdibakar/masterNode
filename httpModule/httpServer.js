6d935992ad8f4d8cbc41390de73a32a64015ef0b
DIBAKAR DAS <careofdibakar@gmail.com>
2024-04-04 14:34:48 +0530

    URL module implemented

--- httpModule/httpServer.js
+++
@@ -1,8 +0,0 @@
-const { log } = require('console');
-const http = require('http');
-
-const server = http.createServer((request, response) => {
-    response.end('Server Created');
-})
-
-module.exports = server;
--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const { log } = require('console');
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((request, response) => {
-    response.end('Server Created');
+    let requestURL = url.parse(request.url, true);
+    // response.write(requestURL.path); 
+    // response.write(requestURL.host); 
+    switch (requestURL.path) {
+        case '/home':
+            response.write('This is home page');
+            break;
+        case '/contact_us':
+            response.write('This is contact page');
+            break;
+        case '/user':
+            response.write('This is user page');
+            break;
+    };
+    response.end();
 }).listen(8000, ()=>{
     log('Server listening at 8000');
 })
